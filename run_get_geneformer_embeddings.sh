06dde377eb3ce18571f0c318eaf4c978dfcc3eb4
briannaflynn <brianna.flynn@utexas.edu>
2025-05-13 22:31:38 +0000

    stratify by idx and modeling pipeline

---
+++ run_get_geneformer_embeddings.sh
@@ -0,0 +1,9 @@
+python ./gene_embedding_analysis/get_geneformer_embeddings.py ./GF-12L30M/GF-12L30M_HUMANgenelist.txt ./GF-12L30M/GF-12L30M_HUMANemb.csv
+
+python ./gene_embedding_analysis/get_geneformer_embeddings.py ./GF-12L95MCANCER/GF-12L95MCANCER_UNIPROT_HUMANgenelist.txt ./GF-12L95MCANCER/GF-12L95MCANCER_UNIPROT_HUMANemb.csv
+
+python ./gene_embedding_analysis/get_geneformer_embeddings.py ./GF-12L95M/GF-12L95M_HUMANgenelist.txt ./GF-12L95M/GF-12L95M_HUMANemb.csv 
+
+python ./gene_embedding_analysis/get_geneformer_embeddings.py ./GF-20L95M/GF-20L95M_HUMANgenelist.txt ./GF-20L95M/GF-20L95M_HUMANemb.csv
+
+python ./gene_embedding_analysis/get_geneformer_embeddings.py ./GF-6L30M/GF-6L30M_HUMANgenelist.txt ./GF-6L30M/GF-6L30M_HUMANemb.csv
